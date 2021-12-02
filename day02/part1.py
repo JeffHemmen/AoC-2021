017cb9067fa85fcd20c4b4e607746944f79388ad
Jeff Hemmen <com.github@citrusfruit.lu>
2021-12-03 00:02:38 +0100

    Day02Part1: Solved

--- day02/part1.py
+++ day02/part1.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 def main(input):
-    raise NotImplementedError
+    h, d = 0, 0
+    for instr, n in input:
+        if instr == 'forward':
+            h += n
+        elif instr == 'down':
+            d += n
+        elif instr == 'up':
+            d -= n
+    return h*d
 
 if __name__ == '__main__':
     with open('input.txt', 'r') as f:
