b603e61f384386a45298beeb920dc84fbd55295b
Jeff Hemmen <com.github@citrusfruit.lu>
2021-12-03 10:08:00 +0100

    Day03Part1: Solved

--- day03/part1.py
+++ day03/part1.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 
 def main(input):
-    raise NotImplementedError
+    sumbits = [0] * len(input[0])
+    for bits in input:
+        for i, b in enumerate(bits):
+            if b == '1':
+                sumbits[i] += 1
+    # print(sumbits)
+    # GAMMA - MOST COMMON BITS
+    gamma, epsilon = 0, 0
+    n = len(input)
+    for pwr, bit in enumerate(reversed(sumbits)):
+        if bit > n / 2: # most common bit 1
+            gamma += 2 ** pwr
+        else:
+            epsilon += 2 ** pwr
+    return epsilon * gamma
 
 if __name__ == '__main__':
     with open('input.txt', 'r') as f:
--- day03/test_part1.py
+++ day03/test_part1.py
@@ -1,6 +1,6 @@
 import part1
 
 def test_with_example_input():
-    with open('input.txt', 'r') as f:
+    with open('test.1.input.txt', 'r') as f:
         input = [line for line in f.read().split()]
     assert part1.main(input) == 198
